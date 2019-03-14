e81536bbc882353acc7e8fe9e28255f8c088a2e3
KAAAsS <admin@kaaass.net>
2019-03-14 23:14:10 +0800

    Add unit test of fibonacci

--- test/test_fibonacci.c
+++ test/test_fibonacci.c
@@ -2,3 +2,19 @@
 // Created by guyis on 2019/3/14.
 //
 
+#include "unity.h"
+#include "../demo/fibonacci.c"
+
+void test_fibonacci() {
+    TEST_ASSERT_EQUAL(1, fib(0));
+    TEST_ASSERT_EQUAL(1, fib(1));
+    TEST_ASSERT_EQUAL(233, fib(12));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_fibonacci);
+
+    return UNITY_END();
+}
